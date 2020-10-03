7396ebc49e73cd8dd424225b365fe998bf6b1a71
Kanishk-kumar399 <kanishkk301198@gmail.com>
2020-10-03 11:33:52 +0530

    [kumar kanishk] added UC5:new feature for password

--- UserRegistration.java
+++ UserRegistration.java
@@ -2,19 +2,19 @@ import java.util.*;
 public class UserRegistration
 {
 	Scanner sc=new Scanner(System.in);
-	public void validMobileNumber()
+	public void validPassword()
 	{
-		System.out.println("Enter the Mobile Number");
-		String mobNumber=sc.nextLine();
-		String regex="^(\\d{2})\\s{1}\\d{10}";
-		if(mobNumber.matches(regex))
-			System.out.println("Mobile Number is correct.");
+		System.out.println("Enter the Password");
+		String password=sc.nextLine();
+		String regex=".{8,}";
+		if(password.matches(regex))
+			System.out.println("Password is correct.");
 		else
-			System.out.println("Mobile Number is Incorrect.");
+			System.out.println("Password is Incorrect.");
 	}
 	public static void main(String args[])
 	{
 	UserRegistration user=new UserRegistration();
-	user.validMobileNumber();
+	user.validPassword();
 	}
 }
