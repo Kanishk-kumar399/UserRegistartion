6265a6a1d94d1ebb0230ef22b7bcfc3c3cafef12
Kanishk-kumar399 <kanishkk301198@gmail.com>
2020-10-03 11:14:52 +0530

    [Kumar Kanishk] adde UC3: new feature for Emial id

--- UserRegistration.java
+++ UserRegistration.java
@@ -2,19 +2,19 @@ import java.util.*;
 public class UserRegistration
 {
 	Scanner sc=new Scanner(System.in);
-	public void validLastName()
+	public void validEmail()
 	{
-		System.out.println("Enter the Last Name");
-		String name=sc.next();
-		String regex="^[A-Z][a-z]{2,}";
-		if(name.matches(regex))
-			System.out.println("Last Name is correct.");
+		System.out.println("Enter the EmailId");
+		String emailId=sc.next();
+		String regex="[\\w-\\+]+(\\.[\\w]+)*@[\\w-]+(\\.[\\w]+)*(\\.[a-z]{2,})$";
+		if(emailId.matches(regex))
+			System.out.println("EmailId is correct.");
 		else
-			System.out.println("Last Name is Incorrect.");
+			System.out.println("EmailId is Incorrect.");
 	}
 	public static void main(String args[])
 	{
 	UserRegistration user=new UserRegistration();
-	user.validLastName();
+	user.validEmail();
 	}
 }
