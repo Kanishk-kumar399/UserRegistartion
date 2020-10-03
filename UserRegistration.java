e270f0fb6c19903e5c8cfd95b4f5e6d48204e495
Kanishk-kumar399 <kanishkk301198@gmail.com>
2020-10-03 11:23:32 +0530

    [kumar kanishk] added UC4 branch:new feature for Mobile Number

--- UserRegistration.java
+++ UserRegistration.java
@@ -2,19 +2,19 @@ import java.util.*;
 public class UserRegistration
 {
 	Scanner sc=new Scanner(System.in);
-	public void validEmail()
+	public void validMobileNumber()
 	{
-		System.out.println("Enter the EmailId");
-		String emailId=sc.next();
-		String regex="[\\w-\\+]+(\\.[\\w]+)*@[\\w-]+(\\.[\\w]+)*(\\.[a-z]{2,})$";
-		if(emailId.matches(regex))
-			System.out.println("EmailId is correct.");
+		System.out.println("Enter the Mobile Number");
+		String mobNumber=sc.nextLine();
+		String regex="^(\\d{2})\\s{1}\\d{10}";
+		if(mobNumber.matches(regex))
+			System.out.println("Mobile Number is correct.");
 		else
-			System.out.println("EmailId is Incorrect.");
+			System.out.println("Mobile Number is Incorrect.");
 	}
 	public static void main(String args[])
 	{
 	UserRegistration user=new UserRegistration();
-	user.validEmail();
+	user.validMobileNumber();
 	}
 }
